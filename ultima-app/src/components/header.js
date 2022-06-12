1965de73108bf775838b5788eb74b6523e7aa044
Abidan Taxitiemuer <abida@Abidans-MBP.eduroam.gmu.edu>
2022-06-12 17:47:16 -0400

    header component built

---
+++ ultima-app/src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './header.css';
+
+function header() {
+  return (
+    <div className="header">
+      <h1>SFPOPOS</h1>
+            <div className="Title-Subtitle">San Franciscos Privately Owned Public Spaces</div>
+    </div>
+  )
+}
+
+export default header
