4b58e70a9eb3790a6388364a97dd59c20b0af664
jobas <jobas@gmu.edu>
2022-06-21 23:44:36 -0400

    Testing changes

--- test/Ultima.js
+++ test/Ultima.js
@@ -4,12 +4,16 @@ const { ethers } = require("hardhat");
 
 describe("Ultima", function () {
   
-  let token; // = 0x514910771af9ca656af840dff83e8264ecf986can;
-  
+  const tokenContract1 = "0x514910771AF9Ca656af840dff83E8264EcF986CA";
+  const tokenContract2 = "0xD76b5c2A23ef78368d8E34288B5b65D616B746aE";
+  let owner;
+  let addr1;
+
   beforeEach(async function () {
     // Get the ContractFactory and Signers here.
     const Ultima = await ethers.getContractFactory("Ultima");
-    const [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+    [owner,addr1] = await ethers.getSigners();
+    
 
     // To deploy our contract, we just have to call Token.deploy() and await
     // for it to be deployed(), which happens once its transaction has been
@@ -18,9 +22,10 @@ describe("Ultima", function () {
   });
   
   it("Should return the balance", async function () {
-    
-    const tokenContract = "0x514910771AF9Ca656af840dff83E8264EcF986CA";
-    expect(await contract.getBalance(tokenContract)).to.equal(0);
+
+    expect(await contract.getBalance(tokenContract1)).to.equal(0);
+    expect(await contract.getBalance(tokenContract2)).to.equal(0);
+
 
   });
 });
